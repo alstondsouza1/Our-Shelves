f27a612d1107f6d9fb51c618fc7513d5b83f8095
TavSingh <tavparsadsingh2@gmail.com>
2025-11-09 14:16:18 -0800

    Working on first test

--- backend/__tests__/bookController.test.js
+++ backend/__tests__/bookController.test.js
@@ -1,4 +1,4 @@
-import { expect, jest } from '@jest/globals';
+import { afterAll, beforeEach, expect, jest } from '@jest/globals';
 import { fetchBooks } from '../controllers/bookController';
 import { describe } from 'pm2';
 
@@ -12,10 +12,36 @@ const mockResponse = () => {
     return res;
 }
 
-const consoleErrSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+const consoleErrSpy = jest.spyOn(console, "error").mockImplementation(() => { });
 
-describe('fetchBooks', () => {
-    it("Fetching books from api", () => {
-        expect(true).toEqual(true);
+describe("fetchBooks", () => {
+    beforeEach(() => {
+        fetch.mockClear();
+        consoleErrSpy.mockClear();
+    });
+
+    afterAll(() => {
+        global.fetch = ogFetch;
+        consoleErrSpy.mockRestore();
+    });
+
+    it("should fetch books, encode URL, and map data correctly", () => {
+        const bookName = "Moby Dick";
+        const mockData = {
+            numFound: 1,
+            docs: [{
+                title: 'Moby Dick',
+                author_name: ['Herman Melville'],
+                first_publish_year: 1851,
+                cover_i: 98765,
+                number_of_pages_median: 634
+            }]
+        };
+
+        fetch.mockResolvedValueOnce({
+            ok: true,
+            status: 200,
+            json: async () => mockData,
+        });
     });
 });
