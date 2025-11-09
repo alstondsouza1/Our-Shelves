92e1d44469890c8fd88d561d935ed00101f0cd24
TavSingh <tavparsadsingh2@gmail.com>
2025-11-09 14:10:56 -0800

    Adding testing for bookController

--- backend/__tests__/bookController.test.js
+++ backend/__tests__/bookController.test.js
@@ -2,6 +2,18 @@ import { expect, jest } from '@jest/globals';
 import { fetchBooks } from '../controllers/bookController';
 import { describe } from 'pm2';
 
+const ogFetch = global.fetch;
+global.fetch = jest.fn();
+
+const mockResponse = () => {
+    const res = {};
+    res.status = jest.fn().mockReturnThis();
+    res.json = jest.fn().mockReturnThis();
+    return res;
+}
+
+const consoleErrSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
 describe('fetchBooks', () => {
     it("Fetching books from api", () => {
         expect(true).toEqual(true);
