87c7578e49b84da74c24b549767a824b8ef5dafa
TavSingh <tavparsadsingh2@gmail.com>
2025-11-12 12:59:11 -0800

    Added a general routes api test.

--- backend/tests/books.integration.test.js
+++ backend/tests/books.integration.test.js
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { jest } from "@jest/globals";
+import { afterAll, beforeAll, jest } from "@jest/globals";
 import app from "../app.js";
 import db from "../db.js";
 
@@ -23,3 +23,30 @@ global.fetch = jest.fn(() =>
         json: () => Promise.resolve(mockApiResponse),
     })
 );
+
+let testBookId;
+
+beforeAll(async () => {
+    await db.execute("TRUNCATE Table books")
+});
+
+afterAll(async () => {
+    await db.execute('TRUNCATE TABLE books');
+    await db.end();
+});
+
+describe('App & General Routes Integration Tests', () => {
+
+  test('GET / - Should return the welcome message', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('Welcome to Our Shelves API!');
+  });
+
+  test('GET /db-test - Should return database connection success and books array', async () => {
+    const response = await request(app).get('/db-test');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe('Database connected');
+    expect(Array.isArray(response.body.books)).toBe(true);
+  });
+});
